9e9fe7e216cef4583b125efdbdc3260ed2b3cf4d
Kimberly_Glick <kimberlyjglick@gmail.com>
2020-09-08 22:21:11 -0400

    All components built and integrated

--- codecademy-ravenous/ravenous/src/components/BusinessList/BusinessList.js
+++ codecademy-ravenous/ravenous/src/components/BusinessList/BusinessList.js
@@ -4,15 +4,17 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render() {
-    <div className="BusinessList">
-      <Business /> 
-      <Business /> 
-      <Business /> 
-      <Business /> 
-      <Business /> 
-      <Business /> 
-    </div>
-  }
+    return (
+      <div className="BusinessList">
+        <Business /> 
+        <Business /> 
+        <Business /> 
+        <Business /> 
+        <Business /> 
+        <Business /> 
+      </div>
+    )
+  };
 }
 
 export default BusinessList; 
